815924eb858c3b3fb09ee6afbf85deb988b543a0
Carlos Alberto Rodríguez Benjume <carlos.alberto.rb97@gmail.com>
2022-11-29 16:54:10 -0600

    Login and Users Session

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import ListOfGifs from 'components/ListOfGifs'
-import {useGifs} from 'Hooks/useGifs'
+import { useGifs } from 'Hooks/useGifs'
 import TrendingSearches from 'components/TrendingSearches'
 import SearchForm from 'components/SearchForm'
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 export default function Home() {
-  const {gifs} = useGifs()
+  const { gifs } = useGifs()
 
   return (
     <>
